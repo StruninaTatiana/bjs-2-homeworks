50f9c471d954a69a40ed3e7fbd575150fb93f102
StruninaTatiana <struninata@mail.ru>
2022-06-07 00:01:53 +0300

    final edit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,10 @@
 function compareArrays(arr1, arr2) {
-  let result = false;
-  
-  if (arr1.length !== arr2.length) {
-    return result;
-  }
-
-  result = arr1.every((item, idx) => item === arr2[idx]);
-  return result; 
+  return arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]);; 
 }
 
 function advancedFilter(arr) {
   let resultArr = arr.filter((item) => item > 0)
     .filter((item) => item % 3 === 0)
-    .map((item) => item *= 10);
+    .map((item) => item * 10);
   return resultArr; 
 }
