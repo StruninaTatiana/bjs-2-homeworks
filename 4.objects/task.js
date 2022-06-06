05b515879d93d5cb3608f74ab3c602444ac431bd
StruninaTatiana <struninata@mail.ru>
2022-06-06 23:53:28 +0300

    First commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;  
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined){ 
+    this.marks = [];
+  }
+
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined){ 
+    this.marks = [];
+  }
+
+  for (let i = 0; i < marks.length; i++) {
+    this.marks.push(marks[i]);
+  }
+}
+
+Student.prototype.getAverage = function (...marks) {
+  let sum = 0;
+
+  if (this.marks === undefined){ 
+    this.marks = [];
+  }
+
+  this.marks.forEach (function (elem) {
+    sum += elem;
+  });
+
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
