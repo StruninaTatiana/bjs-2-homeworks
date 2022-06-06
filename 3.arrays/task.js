7f76f807bab51e20bbb966360f9f87615a098f77
StruninaTatiana <struninata@mail.ru>
2022-06-07 00:06:32 +0300

    last edit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -3,8 +3,7 @@ function compareArrays(arr1, arr2) {
 }
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter((item) => item > 0)
+  return arr.filter((item) => item > 0)
     .filter((item) => item % 3 === 0)
     .map((item) => item * 10);
-  return resultArr; 
 }
