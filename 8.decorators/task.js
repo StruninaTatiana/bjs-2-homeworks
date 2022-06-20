5c4212bba1ca445f84d23d0698ef26157c35e045
StruninaTatiana <struninata@mail.ru>
2022-06-20 19:38:57 +0300

    First commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,69 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper (...args) {
+    const hash = args.join(',');
+
+    let ind = cache.findIndex((item) => item.hash === hash);
+
+      if (ind >= 0) {
+        console.log ('Из кэша: ' + cache[ind].result);
+      } else {
+        let result = func.call(this, ...args);
+        
+        if (cache.length > 4) {
+          cache.shift();
+        };
+      
+        cache.push({hash: hash, result: result});
+        console.log('Вычисляем: ' + result);
+      };
+  };
+  return String(wrapper);
+};
+
+
+function debounceDecoratorNew(func, ms) { 
+  let timeout;   
+
+  return function (...args) { 
+    console.time('time');
+
+    if (!timeout) {
+      func.apply(this, args);
+    };
+
+    clearTimeout(timeout); 
+
+    timeout = setTimeout(() => {            
+      console.timeEnd("time");      
+    }, ms);    
+  };       
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+
+function debounceDecorator2(func, ms) {
+  let timeout; 
+  
+  function wrapper(...args) {   
+    console.time('time');
+    wrapper.count += 1;
+
+    if (!timeout) {
+      func.apply(this, args);
+    };
+
+    clearTimeout(timeout); 
+
+    timeout = setTimeout(() => {            
+      console.timeEnd("time");      
+    }, ms);    
+  }; 
+    
+  wrapper.count = 0;
+  console.log('wrapper.count  ' + wrapper.count)
+  return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
-}
+
